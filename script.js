bede07bfe88c449f0d8022be3e9e83d54532fdb0
gandor999 <geodorruales@gmail.com>
2021-11-08 15:26:22 +0800

    Add setTimeout function for loading the content-section

--- script.js
+++ script.js
@@ -23,9 +23,67 @@ setTimeout(() => {
 
 // Begin loading cards and images
 
+const body = document.body;
+const section = document.createElement('section');
+const divRow = document.createElement('div');
 
+section.setAttribute('id', 'content-section');
+section.setAttribute('class', 'container-fluid d-flex justify-content-around justify-content-center mt-5');
+
+
+body.append(section);
+section.append(divRow);
+
+
+// Load for flags section
 setTimeout(() => {
-	const 
+
+	for(let i = 0; i < data.length; ++i){
+
+		let divCol = document.createElement('div');
+		let divCardBody = document.createElement('div');
+		let h4Title = document.createElement('h4');
+		let pTimeZone = document.createElement('p');
+		let pCountryCode = document.createElement('p');
+		let pCallingCode = document.createElement('p');
+		let button = document.createElement('button');
+		let img = document.createElement('img');
+
+
+		divRow.setAttribute('class', 'row');
+		divCol.setAttribute('class', 'card mt-5 col-md-4 col-sm-6');
+		divCardBody.setAttribute('class', 'card-body')
+
+		img.setAttribute('src', data[i].flag);
+		img.setAttribute('class', 'card-img-top rounded');
+
+		h4Title.setAttribute('class', 'card-title d-flex justify-content-center');
+		h4Title.innerText = data[i].name;
+
+		pTimeZone.setAttribute('class', 'card-text');
+		pTimeZone.innerText = `Time Zones: ${data[i].timezones}`;
+		pCountryCode.innerText = `ISO Code: ${data[i].alpha2Code}`;
+		pCallingCode.innerText = `Calling Codes: ${data[i].callingCodes}`;
+
+		button.setAttribute('type', 'button');
+		button.setAttribute('class', 'btn btn-primary offset-10');
+		button.setAttribute('data-toggle', 'modal');
+		button.setAttribute('data-target', '#exampleModal');
+		button.innerText = 'Details';
+
+		
+		divRow.append(divCol);
+		divCol.append(img);
+		divCol.append(divCardBody);
+		divCardBody.append(h4Title);
+		divCardBody.append(pTimeZone);
+		divCardBody.append(pCountryCode);
+		divCardBody.append(pCallingCode);
+		divCardBody.append(button);
+	}
+
+	
+
 }, 1000);
 
 
